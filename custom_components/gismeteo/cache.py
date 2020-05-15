ccc1321dbd346b5d34ae3d9dee930b0a750b6959
Andrey Khrolenok <andrey@khrolenok.ru>
2020-05-15 17:17:12 +0300

    Rebuild development environment

--- custom_components/gismeteo/cache.py
+++ custom_components/gismeteo/cache.py
@@ -2,6 +2,7 @@
 #  Copyright (c) 2019, Andrey "Limych" Khrolenok <andrey@khrolenok.ru>
 #
 # Version 3.0
+"""Cache controller."""
 
 import logging
 import os
@@ -11,25 +12,29 @@ _LOGGER = logging.getLogger(__name__)
 
 
 class Cache:
+    """Data caching class."""
+
     def __init__(self, params=None):
-        _LOGGER.debug('Initializing cache')
+        """Initialize cache."""
+        _LOGGER.debug("Initializing cache")
         params = params or {}
 
-        self._cache_dir = params.get('cache_dir', '')
-        self._cache_time = params.get('cache_time', 0)
-        self._domain = params.get('domain')
+        self._cache_dir = params.get("cache_dir", "")
+        self._cache_time = params.get("cache_time", 0)
+        self._domain = params.get("domain")
 
         if self._cache_dir:
             self._cache_dir = os.path.abspath(self._cache_dir)
 
-        if params.get('clean_dir', False):
+        if params.get("clean_dir", False):
             self._clean_dir()
 
     def _clean_dir(self):
+        """Clean cache."""
         now_time = time.time()
 
         if self._cache_dir and os.path.exists(self._cache_dir):
-            _LOGGER.debug('Cleaning cache directory %s', self._cache_dir)
+            _LOGGER.debug("Cleaning cache directory %s", self._cache_dir)
             files = os.listdir(self._cache_dir)
             _LOGGER.debug(files)
             for file_name in files:
@@ -42,16 +47,17 @@ class Cache:
                     pass
 
     def _get_file_path(self, file_name):
+        """Get path of cache file."""
         if self._domain:
-            file_name = '.'.join((self._domain, file_name))
+            file_name = ".".join((self._domain, file_name))
         return os.path.join(self._cache_dir, file_name)
 
     def is_cached(self, file_name):
+        """Return True if cache file is exists."""
         result = False
 
         file_path = self._get_file_path(file_name)
-        if os.path.exists(file_path) \
-                and os.path.isfile(file_path):
+        if os.path.exists(file_path) and os.path.isfile(file_path):
             file_time = os.path.getmtime(file_path)
             now_time = time.time()
 
@@ -60,8 +66,9 @@ class Cache:
         return result
 
     def read_cache(self, file_name):
+        """Read cached data."""
         file_path = self._get_file_path(file_name)
-        _LOGGER.debug('Read cache file %s', file_path)
+        _LOGGER.debug("Read cache file %s", file_path)
         if self.is_cached(file_name):
             file = open(file_path)
             content = file.read()
@@ -72,13 +79,14 @@ class Cache:
         return content
 
     def save_cache(self, file_name, content):
+        """Save data to cache."""
         if self._cache_dir:
             if not os.path.exists(self._cache_dir):
                 os.makedirs(self._cache_dir)
 
             file_path = self._get_file_path(file_name)
-            _LOGGER.debug('Store cache file %s', file_path)
+            _LOGGER.debug("Store cache file %s", file_path)
 
             file = open(file_path, "w")
-            file.write(content.decode('utf-8'))
+            file.write(content.decode("utf-8"))
             file.close()
--- custom_components/gismeteo/const.py
+++ custom_components/gismeteo/const.py
@@ -15,42 +15,63 @@ from datetime import timedelta
 from homeassistant.components.weather import ATTR_FORECAST_CONDITION
 
 # Base component constants
-DOMAIN = 'gismeteo'
-VERSION = '2.0.14'
+DOMAIN = "gismeteo"
+VERSION = "2.0.14"
 ISSUE_URL = "https://github.com/Limych/ha-gismeteo/issues"
-ATTRIBUTION = 'Data provided by Gismeteo'
+ATTRIBUTION = "Data provided by Gismeteo"
 
-BASE_URL = 'https://services.gismeteo.ru/inform-service/inf_chrome'
+BASE_URL = "https://services.gismeteo.ru/inform-service/inf_chrome"
 
 MMHG2HPA = 1.333223684
 MS2KMH = 3.6
 
-CONF_CACHE_DIR = 'cache_dir'
+CONF_CACHE_DIR = "cache_dir"
 
-FORECAST_MODE_HOURLY = 'hourly'
-FORECAST_MODE_DAILY = 'daily'
+FORECAST_MODE_HOURLY = "hourly"
+FORECAST_MODE_DAILY = "daily"
 
-DEFAULT_NAME = 'Gismeteo'
+DEFAULT_NAME = "Gismeteo"
 
 MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=5)
 
-CONDITION_FOG_CLASSES = [11, 12, 28, 40, 41, 42, 43, 44, 45, 46, 47, 48,
-                         49, 120, 130, 131, 132, 133, 134, 135, 528]
+CONDITION_FOG_CLASSES = [
+    11,
+    12,
+    28,
+    40,
+    41,
+    42,
+    43,
+    44,
+    45,
+    46,
+    47,
+    48,
+    49,
+    120,
+    130,
+    131,
+    132,
+    133,
+    134,
+    135,
+    528,
+]
 
-ATTR_SUNRISE = 'sunrise'
-ATTR_SUNSET = 'sunset'
+ATTR_SUNRISE = "sunrise"
+ATTR_SUNSET = "sunset"
 
 ATTR_WEATHER_CONDITION = ATTR_FORECAST_CONDITION
-ATTR_WEATHER_CLOUDINESS = 'cloudiness'
-ATTR_WEATHER_PRECIPITATION_TYPE = 'precipitation_type'
-ATTR_WEATHER_PRECIPITATION_AMOUNT = 'precipitation_amount'
-ATTR_WEATHER_PRECIPITATION_INTENSITY = 'precipitation_intensity'
-ATTR_WEATHER_STORM = 'storm'
-ATTR_WEATHER_GEOMAGNETIC_FIELD = 'gm_field'
-ATTR_WEATHER_PHENOMENON = 'phenomenon'
+ATTR_WEATHER_CLOUDINESS = "cloudiness"
+ATTR_WEATHER_PRECIPITATION_TYPE = "precipitation_type"
+ATTR_WEATHER_PRECIPITATION_AMOUNT = "precipitation_amount"
+ATTR_WEATHER_PRECIPITATION_INTENSITY = "precipitation_intensity"
+ATTR_WEATHER_STORM = "storm"
+ATTR_WEATHER_GEOMAGNETIC_FIELD = "gm_field"
+ATTR_WEATHER_PHENOMENON = "phenomenon"
 
-ATTR_FORECAST_HUMIDITY = 'humidity'
-ATTR_FORECAST_PRESSURE = 'pressure'
+ATTR_FORECAST_HUMIDITY = "humidity"
+ATTR_FORECAST_PRESSURE = "pressure"
 ATTR_FORECAST_CLOUDINESS = ATTR_WEATHER_CLOUDINESS
 ATTR_FORECAST_PRECIPITATION_TYPE = ATTR_WEATHER_PRECIPITATION_TYPE
 ATTR_FORECAST_PRECIPITATION_AMOUNT = ATTR_WEATHER_PRECIPITATION_AMOUNT
