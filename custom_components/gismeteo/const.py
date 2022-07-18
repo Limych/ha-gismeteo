62a0c802fc8dac6ef99ec86b331eff6d076b25ac
Andrey Khrolenok <andrey@khrolenok.ru>
2022-07-19 02:51:40 +0300

    Fix hassfest error 'Do not set title of step user if it's a brand name'

--- custom_components/gismeteo/const.py
+++ custom_components/gismeteo/const.py
@@ -33,7 +33,7 @@ from homeassistant.const import (
 # Base component constants
 NAME: Final = "Gismeteo"
 DOMAIN: Final = "gismeteo"
-VERSION: Final = "2.4.1"
+VERSION: Final = "2.4.2-alpha"
 ATTRIBUTION: Final = "Data provided by Gismeteo"
 ISSUE_URL: Final = "https://github.com/Limych/ha-gismeteo/issues"
 
