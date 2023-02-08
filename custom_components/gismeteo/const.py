97596d3fe962b352c7a93a17f650d054f231448d
Andrey Khrolenok <andrey@khrolenok.ru>
2023-02-09 00:18:04 +0300

    Bump version to 2.5.1

--- custom_components/gismeteo/const.py
+++ custom_components/gismeteo/const.py
@@ -31,7 +31,7 @@ from homeassistant.const import (
 # Base component constants
 NAME: Final = "Gismeteo"
 DOMAIN: Final = "gismeteo"
-VERSION: Final = "2.5.1-alpha"
+VERSION: Final = "2.5.1"
 ATTRIBUTION: Final = "Data provided by Gismeteo"
 ISSUE_URL: Final = "https://github.com/Limych/ha-gismeteo/issues"
 
